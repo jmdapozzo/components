3433e73d750d94e6944e01209ac1666a894c02d8
jmdapozzo <jmdapozzo@macdap.com>
2025-05-22 16:54:43 -0400

    Cleanup mbi5026 example app

--- ledPanel/examples/mbi5026/main/main.cpp
+++ ledPanel/examples/mbi5026/main/main.cpp
@@ -11,7 +11,6 @@ extern "C" void app_main(void)
     const lvgl_port_cfg_t lvglConfig = ESP_LVGL_PORT_INIT_CONFIG();
     lvgl_port_init(&lvglConfig);
 
-    // macdap::LedPanel ledPanel(macdap::w16h8, CONFIG_LED_PANEL_WIDTH, CONFIG_LED_PANEL_HEIGHT);
     macdap::LedPanel &ledPanel = macdap::LedPanel::getInstance();
     ledPanel.setBrightness(1.00);
     ledPanel.message("-.-");
@@ -20,6 +19,7 @@ extern "C" void app_main(void)
 
     while (true)
     {
+        ESP_LOGI(TAG, "LED Panel Test Program");
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
