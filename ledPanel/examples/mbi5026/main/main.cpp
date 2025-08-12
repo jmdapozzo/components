bacabbe3b49e76550d5e1c54ad3e35fed4833921
jmdapozzo <jmdapozzo@macdap.com>
2025-08-12 18:19:07 -0400

    Updated  ledPanel example to new Graphics interface

--- ledPanel/examples/mbi5026/main/main.cpp
+++ ledPanel/examples/mbi5026/main/main.cpp
@@ -6,10 +6,11 @@
 
 static const char *TAG = "mbi5026";
 
-static lv_anim_t _animationTemplate;
-static lv_style_t _styleSmallPrimary;
-static lv_style_t _styleMediumPrimary;
-static lv_style_t _styleMediumAnimation;
+static lv_anim_t _animation;
+static lv_style_t _styleBase;
+static lv_style_t _styleGreetingTop;
+static lv_style_t _styleGreetingBottom;
+static lv_style_t _styleMainMessage;
 
 extern "C" void app_main(void)
 {
@@ -21,23 +22,31 @@ extern "C" void app_main(void)
     lv_display_t *lvDisplay = ledPanel.getLvDisplay();
     graphics.init(lvDisplay);
 
-    lv_anim_init(&_animationTemplate);
-    lv_anim_set_delay(&_animationTemplate, 1000);
-    lv_anim_set_repeat_delay(&_animationTemplate, 3000);
-    lv_anim_set_repeat_count(&_animationTemplate, LV_ANIM_REPEAT_INFINITE);
+    int32_t width = lv_display_get_horizontal_resolution(lvDisplay);
+    int32_t height = lv_display_get_vertical_resolution(lvDisplay);
 
-    lv_style_init(&_styleSmallPrimary);
-    lv_style_set_text_font(&_styleSmallPrimary, &nothingFont5x7);
-    lv_style_set_text_color(&_styleSmallPrimary, lv_color_white());
+    lv_anim_init(&_animation);
+    lv_anim_set_delay(&_animation, 5000);
+    lv_anim_set_repeat_delay(&_animation, 3000);
+    lv_anim_set_repeat_count(&_animation, LV_ANIM_REPEAT_INFINITE);
 
-    lv_style_init(&_styleMediumPrimary);
-    lv_style_set_text_font(&_styleMediumPrimary, &lv_font_unscii_8);
-    lv_style_set_text_color(&_styleMediumPrimary, lv_color_white());
-    
-    lv_style_init(&_styleMediumAnimation);
-    lv_style_set_text_font(&_styleMediumAnimation, &lv_font_unscii_8);
-    lv_style_set_text_color(&_styleMediumAnimation, lv_color_white());
-    lv_style_set_anim(&_styleMediumAnimation, &_animationTemplate);
+    lv_style_init(&_styleBase);
+    lv_style_set_text_color(&_styleBase, lv_color_white());
+    lv_style_set_text_align(&_styleBase, LV_TEXT_ALIGN_CENTER);
+    lv_style_set_align(&_styleBase, LV_ALIGN_CENTER);
+    lv_style_set_width(&_styleBase, width);
+    lv_style_set_anim(&_styleBase, &_animation);
+
+    lv_style_copy(&_styleGreetingTop, &_styleBase);
+    lv_style_set_text_font(&_styleGreetingTop, &nothingFont5x7);
+    lv_style_set_align(&_styleGreetingTop, LV_ALIGN_TOP_LEFT);
+
+    lv_style_copy(&_styleGreetingBottom, &_styleBase);
+    lv_style_set_text_font(&_styleGreetingBottom, &nothingFont5x7);
+    lv_style_set_align(&_styleGreetingBottom, LV_ALIGN_BOTTOM_LEFT);
+
+    lv_style_copy(&_styleMainMessage, &_styleBase);
+    lv_style_set_text_font(&_styleMainMessage, &lv_font_unscii_8);
 
     graphics.background(lvDisplay, lv_color_black());
 
@@ -48,15 +57,16 @@ extern "C" void app_main(void)
     vTaskDelay(pdMS_TO_TICKS(3000));
     graphics.clear(lvDisplay);
 
-    graphics.greeting(lvDisplay, &_styleSmallPrimary, "mbi5026", "0.0.0");
+    graphics.message(lvDisplay, &_styleGreetingTop, "mbi5026");
+    graphics.message(lvDisplay, &_styleGreetingBottom, "0.0.0");
     vTaskDelay(pdMS_TO_TICKS(3000));
     graphics.clear(lvDisplay);
 
-    graphics.message(lvDisplay, &_styleMediumPrimary, "MacDap");
+    graphics.message(lvDisplay, &_styleMainMessage, "MacDap");
     vTaskDelay(pdMS_TO_TICKS(3000));
     graphics.clear(lvDisplay);
 
-    graphics.scrollingMessageCenter(lvDisplay, &_styleMediumAnimation, "Test program x64y32, Version 0.0.0 from MacDap Inc.");
+    graphics.message(lvDisplay, &_styleMainMessage, "Test program x64y32, Version 0.0.0 from MacDap Inc.");
 
     while (true)
     {
