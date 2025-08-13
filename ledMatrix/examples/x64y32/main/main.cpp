78abe685f7ead22c08128a487f5c02fd4c5c1ab7
jmdapozzo <jmdapozzo@macdap.com>
2025-08-13 16:37:33 -0400

    Update to newer interface

--- ledMatrix/examples/x64y32/main/main.cpp
+++ ledMatrix/examples/x64y32/main/main.cpp
@@ -28,16 +28,13 @@ static lv_palette_t palettes[] = {
     LV_PALETTE_GREY,
 };
 
-static lv_color_t _primaryColor = lv_palette_main(LV_PALETTE_GREEN);
-static lv_color_t _secondaryColor = lv_palette_main(LV_PALETTE_RED);
-static lv_color_t _discreteColor = lv_palette_main(LV_PALETTE_AMBER);
-
-static lv_anim_t _animationTemplate;
-static lv_style_t _styleSmallPrimary;
-static lv_style_t _styleMediumPrimary;
-static lv_style_t _styleLargePrimary;
-static lv_style_t _styleLargeAnimation;
-static lv_style_t _styleExtraLargePrimary;
+static lv_anim_t _animation;
+static lv_style_t _styleBase;
+static lv_style_t _styleGreetingTop;
+static lv_style_t _styleGreetingBottom;
+static lv_style_t _styleMainMessage;
+static lv_style_t _styleMainMessageTop;
+static lv_style_t _styleMainMessageBottom;
 
 extern "C" void app_main(void)
 {
@@ -49,32 +46,47 @@ extern "C" void app_main(void)
     lv_display_t *lvDisplay = ledMatrix.getLvDisplay();
     graphics.init(lvDisplay);
 
-    lv_anim_init(&_animationTemplate);
-    lv_anim_set_delay(&_animationTemplate, 1000);
-    lv_anim_set_repeat_delay(&_animationTemplate, 3000);
-    lv_anim_set_repeat_count(&_animationTemplate, LV_ANIM_REPEAT_INFINITE);
+    int32_t width = lv_display_get_horizontal_resolution(lvDisplay);
+    int32_t height = lv_display_get_vertical_resolution(lvDisplay);
 
-    lv_style_init(&_styleSmallPrimary);
-    lv_style_set_text_font(&_styleSmallPrimary, &lv_font_montserrat_14);
-    lv_style_set_text_color(&_styleSmallPrimary, _discreteColor);
+    lv_anim_init(&_animation);
+    lv_anim_set_delay(&_animation, 1000);
+    lv_anim_set_repeat_delay(&_animation, 3000);
+    lv_anim_set_repeat_count(&_animation, LV_ANIM_REPEAT_INFINITE);
 
-    lv_style_init(&_styleMediumPrimary);
-    lv_style_set_text_font(&_styleMediumPrimary, &lv_font_montserrat_20);
-    lv_style_set_text_color(&_styleMediumPrimary, _secondaryColor);
+    lv_style_init(&_styleBase);
+    lv_style_set_bg_color(&_styleBase, lv_palette_main(LV_PALETTE_BLUE));
+    lv_style_set_arc_color(&_styleBase, lv_palette_main(LV_PALETTE_RED));
+    lv_style_set_text_align(&_styleBase, LV_TEXT_ALIGN_CENTER);
+    lv_style_set_align(&_styleBase, LV_ALIGN_CENTER);
+    lv_style_set_text_color(&_styleBase, lv_palette_main(LV_PALETTE_GREEN));
+    lv_style_set_width(&_styleBase, width);
+    lv_style_set_line_width(&_styleBase, 3);
+    lv_style_set_line_color(&_styleBase, lv_palette_main(LV_PALETTE_BLUE));
+    lv_style_set_line_rounded(&_styleBase, true);
+    lv_style_set_anim(&_styleBase, &_animation);
 
-    lv_style_init(&_styleLargePrimary);
-    lv_style_set_text_font(&_styleLargePrimary, &lv_font_montserrat_24);
-    lv_style_set_text_color(&_styleLargePrimary, _primaryColor);
+    lv_style_copy(&_styleGreetingTop, &_styleBase);
+    lv_style_set_text_font(&_styleGreetingTop, &lv_font_montserrat_14);
+    lv_style_set_text_color(&_styleGreetingTop, lv_palette_main(LV_PALETTE_AMBER));
+    lv_style_set_align(&_styleGreetingTop, LV_ALIGN_TOP_LEFT);
 
-    lv_style_init(&_styleLargeAnimation);
-    lv_style_set_text_font(&_styleLargeAnimation, &lv_font_montserrat_24);
-    lv_style_set_text_color(&_styleLargeAnimation, _primaryColor);
-    lv_style_set_anim(&_styleLargeAnimation, &_animationTemplate);
+    lv_style_copy(&_styleGreetingBottom, &_styleBase);
+    lv_style_set_text_font(&_styleGreetingBottom, &lv_font_montserrat_14);
+    lv_style_set_text_color(&_styleGreetingBottom, lv_palette_main(LV_PALETTE_AMBER));
+    lv_style_set_align(&_styleGreetingBottom, LV_ALIGN_BOTTOM_LEFT);
 
-    lv_style_init(&_styleExtraLargePrimary);
-    lv_style_set_text_font(&_styleExtraLargePrimary, &lv_font_montserrat_28);
-    lv_style_set_text_color(&_styleExtraLargePrimary, _primaryColor);
-   
+    lv_style_copy(&_styleMainMessage, &_styleBase);
+    lv_style_set_text_font(&_styleMainMessage, &lv_font_montserrat_24);
+
+    lv_style_copy(&_styleMainMessageTop, &_styleMainMessage);
+    lv_style_set_align(&_styleMainMessageTop, LV_ALIGN_TOP_LEFT);
+    lv_style_set_text_color(&_styleMainMessageTop, lv_palette_main(LV_PALETTE_GREEN));
+
+    lv_style_copy(&_styleMainMessageBottom, &_styleMainMessage);
+    lv_style_set_align(&_styleMainMessageBottom, LV_ALIGN_BOTTOM_LEFT);
+    lv_style_set_text_color(&_styleMainMessageBottom, lv_palette_main(LV_PALETTE_RED));
+
     graphics.background(lvDisplay, lv_color_black());
 
     float brightness = 100.0;
@@ -84,35 +96,36 @@ extern "C" void app_main(void)
     vTaskDelay(pdMS_TO_TICKS(3000));
     graphics.clear(lvDisplay);
 
-    graphics.greeting(lvDisplay, &_styleSmallPrimary, "Test", "Version 0.0.0");
+    graphics.message(lvDisplay, &_styleGreetingTop, "Test");
+    graphics.message(lvDisplay, &_styleGreetingBottom, "Version 0.0.0");
     vTaskDelay(pdMS_TO_TICKS(3000));
     graphics.clear(lvDisplay);
 
-    graphics.message(lvDisplay, &_styleExtraLargePrimary, "MacDap Inc.");
+    graphics.message(lvDisplay, &_styleMainMessage, "MacDap Inc.");
     vTaskDelay(pdMS_TO_TICKS(3000));
     graphics.clear(lvDisplay);
 
-    graphics.qrcode(lvDisplay);
+    graphics.qrcode(lvDisplay, "https://macdap.com");
     vTaskDelay(pdMS_TO_TICKS(3000));
     graphics.clear(lvDisplay);
 
-    graphics.cross(lvDisplay, lv_palette_main(LV_PALETTE_BLUE));
+    graphics.cross(lvDisplay, &_styleBase);
     vTaskDelay(pdMS_TO_TICKS(3000));
     graphics.clear(lvDisplay);
 
-    graphics.scrollingMessageCenter(lvDisplay, &_styleLargeAnimation, "This is a MacDap application!");
-    vTaskDelay(pdMS_TO_TICKS(10000));
+    graphics.message(lvDisplay, &_styleMainMessage, "This is a MacDap application!");
+    vTaskDelay(pdMS_TO_TICKS(5000));
     graphics.clear(lvDisplay);
 
-    graphics.spinner(lvDisplay);
+    graphics.spinner(lvDisplay, height/2, &_styleBase);
     vTaskDelay(pdMS_TO_TICKS(5000));
     graphics.clear(lvDisplay);
 
     int palette_idx = 0;
-    lv_obj_t *heartbeat = graphics.led(lvDisplay, lv_palette_main(palettes[palette_idx]));
+    lv_obj_t *heartbeat = graphics.led(lvDisplay, 32, lv_palette_main(palettes[palette_idx]));
 
-    graphics.scrollingMessageBottom(lvDisplay, &_styleLargeAnimation, "Test program Version 0.0.0 from MacDap Inc.");
-    graphics.scrollingMessageTop(lvDisplay, &_styleMediumPrimary, "MacDap Inc. the best");
+    graphics.message(lvDisplay, &_styleMainMessageTop, "Test program Version 0.0.0 from MacDap Inc.");
+    graphics.message(lvDisplay, &_styleMainMessageBottom, "MacDap Inc. the best");
 
 #ifdef CONFIG_HEAP_TASK_TRACKING
     esp_dump_per_task_heap_info();
